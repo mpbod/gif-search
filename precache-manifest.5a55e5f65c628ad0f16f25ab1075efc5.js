8fd155cc3fec0e5c55725399726d0843e605840d
mpbod <mpbod59@gmail.com>
2020-05-04 02:02:08 +0700

    Updates

---
+++ precache-manifest.5a55e5f65c628ad0f16f25ab1075efc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af6b524d2584897998dacde61c06b99",
+    "url": "/gif-search/index.html"
+  },
+  {
+    "revision": "ff82a59d401931e2b839",
+    "url": "/gif-search/static/css/main.b750c057.chunk.css"
+  },
+  {
+    "revision": "5201dff26db0acaeecc6",
+    "url": "/gif-search/static/js/2.405766b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gif-search/static/js/2.405766b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff82a59d401931e2b839",
+    "url": "/gif-search/static/js/main.442d4871.chunk.js"
+  },
+  {
+    "revision": "367fc285fc91bbddd12b",
+    "url": "/gif-search/static/js/runtime-main.394d266a.js"
+  }
+]);
